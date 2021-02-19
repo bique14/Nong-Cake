8aa2e5a3f229b9af8f5ae663b99d81a93748dc6a
Katy <peerasorn14@gmail.com>
2021-02-19 12:56:02 +0700

    Install tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import "./App.css";
 import RedVelvet from "./red-velvet/details.json";
 
-console.log(RedVelvet);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="bg-gray-800 h-full">
+      <h1 className="text-red-300 font-bold text-6xl">{RedVelvet.name}</h1>
     </div>
   );
 }
