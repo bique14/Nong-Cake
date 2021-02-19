4fe4f4cf0d45763a7bd985b412fe1d6fb3751df1
Katy <peerasorn14@gmail.com>
2021-02-20 00:06:12 +0700

    Add hover style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import "./App.css";
 import RedVelvet from "./red-velvet/details.json";
+import Irene from "./red-velvet/irene/profile.json";
+import Seulgi from "./red-velvet/seulgi/profile.json";
+import Wendy from "./red-velvet/wendy/profile.json";
+import Joy from "./red-velvet/joy/profile.json";
+import Yeri from "./red-velvet/yeri/profile.json";
+
+function Member({ member }: any): JSX.Element {
+  console.log(member);
+  return (
+    <div className="myDIV">
+      <div className="rounded border bg-red-300 px-4 py-2">{member.name}</div>
+      <div className="hide absolute top-0 right-0 bg-white">
+        <span className="block">stage name : {member.stageName}</span>
+        <span className="block">age : {member.age}</span>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="bg-gray-800 h-full">
-      <h1 className="text-red-300 font-bold text-6xl">{RedVelvet.name}</h1>
+    <div className="h-full flex bg-red-700 justify-center items-center">
+      <Member member={Irene} />
+      <Member member={Seulgi} />
+      <Member member={Wendy} />
+      <Member member={Joy} />
+      <Member member={Yeri} />
     </div>
   );
 }
